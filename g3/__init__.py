c56e5de89ba07d354cd159a489a53a4a9a4a8a81
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2012-01-08 13:14:20 +0200

    logging at INFO

--- g3/__init__.py
+++ g3/__init__.py
@@ -2,9 +2,12 @@ import sys
 import logging
 log = logging.getLogger(__name__)
 
-log.setLevel(logging.DEBUG)
-log.addHandler(logging.StreamHandler())
-logging.getLogger('usb').addHandler(logging.StreamHandler(sys.stderr))
+log.setLevel(logging.INFO)
+_h = logging.StreamHandler(sys.stderr)
+_h.setFormatter(logging.Formatter("%(created)-16.5f %(filename)s:%(lineno)s %(levelname)s %(message)s"))
+log.addHandler(_h)
+
+#logging.getLogger('usb').addHandler(_h)
 
 from camera import Camera, G3Error
 find = Camera.find
