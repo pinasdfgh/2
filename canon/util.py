b24daaa4549f4175f793791f6c2d497a98fa01cb
Kiril Zyapkov <kiril.zyapkov@gmail.com>
2012-01-14 12:38:25 +0200

    work in progress ...

--- canon/util.py
+++ canon/util.py
@@ -2,6 +2,10 @@ import struct
 import string
 from array import array
 import inspect
+from orca.orca import isDiacriticalKey
+import math
+
+ARRAY_FORMAT = [None, 'B', 'H', None, 'I', None, None, None, 'Q']
 
 def extract_string(data, start=0):
     try:
@@ -11,13 +15,13 @@ def extract_string(data, start=0):
     return data[start:start+end].tostring()
 
 def le16toi(raw, start=None):
-    raw = _normalize_array(raw)
+    raw = _normalize_to_string(raw)
     if start is not None:
         raw = raw[start:start+2]
     return struct.unpack('<H', raw)[0]
 
 def le32toi(raw, start=None):
-    raw = _normalize_array(raw)
+    raw = _normalize_to_string(raw)
     if start is not None:
         raw = raw[start:start+4]
     return struct.unpack('<I', raw)[0]
@@ -25,13 +29,14 @@ def le32toi(raw, start=None):
 def itole32a(i):
     return array('B', struct.pack('<I', i))
 
-def _normalize_array(raw):
+def _normalize_to_string(raw):
     if type(raw) is array:
         raw = raw.tostring()
     elif type(raw) is str:
         pass
-    else:
-        raw = array('B', raw).tostring()
+    elif type(raw) in (int, long):
+        length = math.ceil(raw.bit_length() / 8.0)
+        raw = array(ARRAY_FORMAT[length], [raw]).tostring()
     return raw
 
 def chunks(l, n):
@@ -85,10 +90,8 @@ class _BoundFlag(object):
     def __init__(self, bitfield, flag):
         self.bitfield = bitfield
         self.flag = flag
-        self.start = flag.start
-        self.end = flag.end
-        self._pack = flag._pack
-        self._unpack = flag._unpack
+        for attr in ('_start', '_end', '_length', '_pack', '_unpack'):
+            setattr(self, attr, getattr(flag, attr))
 
     @property
     def name(self):
@@ -97,11 +100,17 @@ class _BoundFlag(object):
     def store(self, value):
         """Store the value in the bitfield."""
         value = self._pack(value)
-        self.bitfield[self.start:self.end] = value
+        self._store(value)
+
+    def _store(self, packed):
+        self.bitfield[self._start:self._end] = packed
+
+    def _extract(self):
+        data = self.bitfield[self._start:self._end]
+        return data
 
     def __int__(self):
-        data = self.bitfield[self.start:self.end]
-        return self._unpack(data)
+        return self._unpack(self._extract())
 
     def __hex__(self):
         """hex() needs a push."""
@@ -119,40 +128,76 @@ class _BoundFlag(object):
         self.store(new)
         return self
 
+    def __contains__(self, other):
+        """(VALUE[, VALUE ...]) in self -> all(lambda x: x in self, (VALUE[, ...])
+        """
+        try:
+            all = 0x00
+            for o in other:
+                all |= int(o)
+            return int(self) == all
+        except TypeError:
+            return int(self) == other
+
     def __repr__(self):
         return ("<{} 0x{:x} 0b{:0"+str(self.flag.length)+"b}>").format(self.name, int(self), int(self))
 
 class Flag(object):
     """A set of bitmasks within a bitfield.
+
+    This may represent combinations of bit states between 8 and 64 bits long,
+    instances are descriptors on Bitfield-s.
+
     """
 
-    _formats = [None, 'B', 'H', None, 'I', None, None, None, 'Q']
+    _bound_class = _BoundFlag
 
-    def __init__(self, offset, length=None, fmt=None, **choices):
-        self.start = offset
-        self.length = 1
+    def __init__(self, offset, length=None, fmt=None, mask=None, **choices):
+        self._start = int(offset)
+        self._length = 1
         if length is not None:
             assert length > 0 and length <= 8
-            self.length = int(length)
-        self.end = self.start + self.length
+            self._length = int(length)
+        self._end = self._start + self._length
         if fmt is None:
-            self.fmt = self._formats[self.length]
+            self.fmt = ARRAY_FORMAT[self._length]
         else:
             assert struct.calcsize(fmt) == \
-                    struct.calcsize(self._formats[self.length])
+                    struct.calcsize(ARRAY_FORMAT[self._length])
             self.fmt = fmt
-        self._by_name = choices.copy()
-        self._by_flag = dict((v,k) for (k,v) in choices.items())
         self._bound = {}
+        self._by_name = {}
+        self._by_value = {}
+        if choices:
+            self._by_name = choices.copy()
+            choices.reverse()
+            self._by_value = dict((v,k) for (k,v) in choices.items().reverse())
+
+    def __getattribute__(self, name):
+        by_name = super(Flag, self).__getattribute__('_by_name')
+        by_value = super(Flag, self).__getattribute__('_by_value')
+        if name in by_name:
+            return by_name[name]
+        elif name in by_value:
+            return by_value[name]
+#        elif name in self.__dict__:
+#            return self.__dict__[name]
+        else:
+            return super(Flag, self).__getattribute__(name)
+
+
+    @classmethod
+    def _get_bound_instance(cls, self, bitfield):
+        return cls._bound_class(bitfield, self)
 
     def _get_bound(self, bitfield):
         h = hash(bitfield)
         if h not in self._bound:
-            self._bound[h] = _BoundFlag(bitfield, self)
+            self._bound[h] = self._get_bound_instance(self, bitfield)
         return self._bound[h]
 
     def _unpack(self, data):
-        data = _normalize_array(data)
+        data = _normalize_to_string(data)
         return struct.unpack(self.fmt, data)[0]
 
     def _pack(self, value):
@@ -164,11 +209,27 @@ class Flag(object):
         return self._get_bound(bitfield)
 
     def __set__(self, bitfield, value):
+        if bitfield is None:
+            return
         b = self._get_bound(bitfield)
         b.store(value)
 
-    def __delete__(self):
-        raise RuntimeError("what?")
+class BooleanFlag(Flag):
+    _size = 1
+    class _bound_class(_BoundFlag):
+        def __nonzero__(self):
+            if int(self) == self.choices['true']:
+                return True
+            return False
+
+    def __init__(self, offset=0, length=None, fmt=None, true=0x01, false=0x00):
+        choices = dict(true=true, on=true, y=true, yes=true,
+                       false=false, off=false, n=false, no=false)
+        if true ^ false == 0x00:
+            raise ValueError("Values for true and false must differ")
+
+        super(BooleanFlag, self).__init__(offset, length, fmt, **choices)
+        self._mask = true ^ false
 
 class Bitfield(array):
     """Packs an array('B', ...) as a set of flags
@@ -177,7 +238,8 @@ class Bitfield(array):
     ``cls._size`` to be the length of the bitfield in bytes and any
     number of instances of ``Flag`` as class attributes.
 
-    See ``Flag``.
+    The ``Flag`` instances provide the descriptor protocol and provide
+    convenient access to values of various flag within the array.
 
     """
     _size = None
